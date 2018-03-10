99b45854754ddf3964aa9f4328c91ea8a2e0607c
zhaowang <liang19931@163.com>
2018-03-10 11:15:51 +0800

    Create [ANOJ]上帝视角[二叉树遍历].cpp
    https://logn.me/problem/1013
    Problem Description
    
    给一棵二叉树的层序遍历序列和中序遍历序列，求这棵二叉树的先序遍历序列和后序遍历序列，并给出从右往左、从右上往左下、从上往下分别能看到的结点个数。注意，此处均把二叉树的每条边都设置为等长，角度为45度，因此结点可能在视觉上重叠。所谓从右往左看是指，对同一层的结点，右边的结点会挡住左边的结点，这样同一层结点就只能看到最右边的那一个；同样的，从右上往左下看是指，右上角的结点会挡住左下角45度的结点；从上往下看是指，对同一竖直位置来说，只能看到最上方的结点。
    
    例如对下图来说，从右往左能看到3个结点，从右上往左下能看到3个结点，从上往下能看到5个结点。
    
    
    Input
    
    每个输入文件中一组数据。
    
    第一行一个正整数N（1<=N<=30），代表二叉树的结点个数（结点编号为1~N）。
    
    接下来两行，每行N个正整数，分别代表二叉树的层序遍历序列和中序遍历序列。数据保证序列中1~N的每个数出现且只出现一次。
    Output
    
    先输出两行，每行N个正整数，分别代表二叉树的先序遍历序列和后序遍历序列。
    
    接下来分三行输出从右往左、从右上往左下、从上往下分别能看到的结点个数。
    
    每行末尾均不允许输出多余的空格。
    Sample Input
    
    7
    1 2 3 4 5 6 7
    4 2 5 1 6 3 7
    Sample Output
    
    1 2 4 5 3 6 7
    4 5 2 6 7 3 1
    3
    3
    5

---
+++ [ANOJ]上帝视角[二叉树遍历].cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+/* 表示结点总数， 最小x坐标值，最大x坐标值， 最大y坐标值*/
+int N, minx = 100, mx = -100, my = -1;
+set<int> lrt;	//**用来存储x+y的和
+vector<int> lv, nv, pv, tv;//**层序，中序，先序和后序的容器
+
+void dfs(vector<int> v, int nl, int nr, int x, int y); //**递归处理
+void my_print(vector<int> &v);
+
+/*
+题意：二叉树遍历题。
+思路：
+这题有点难。
+首先要实现层序和中序生成先序和后序。
+层序第一个根结点，在中序里分成左树和右树两部分。
+然而根的左树在层序里是分散开的，该如何递归处理呢？
+于是我们可以把层序中属于左树的元素找出来，右树同理。
+后面就可以递归处理了。
+关于视角个数，我用的坐标处理。
+根标记为原点，往左走x-1，y+1；
+往右走，x+1, y+1;
+显然从右看，y的最大值+1即为个数；
+从上往下，x的最大和最小值差+1即为个数；
+从右上，x+y和的不同个数。
+已AC.
+*/
+
+int main()
+{
+	//freopen("in.txt", "r", stdin);
+	scanf("%d", &N);
+	lv.resize(N);
+	nv.resize(N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &lv[i]);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &nv[i]);
+	dfs(lv, 0, N - 1, 0, 0);
+	my_print(pv);
+	my_print(tv);
+	printf("%d\n%d\n%d\n", my + 1, lrt.size(), mx - minx + 1);
+
+	return 0;
+}
+
+void dfs(vector<int> v, int nl, int nr, int x, int y)
+{
+	if (nl > nr)
+		return;
+
+	int pos = 0, H[33] = { 0 }, S[33] = { 0 };
+	vector<int> left, right;
+	for (int i = nl; i <= nr; i++)
+	{
+
+		if (nv[i] == v[0])
+		{
+			pos = i;
+			break;
+		}
+		else
+		{
+			H[nv[i]] = 1;
+		}
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (H[v[i]] == 1)
+			left.push_back(v[i]);
+	}
+	for (int i = pos + 1; i <= nr; i++)
+	{
+		S[nv[i]] = 1;
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (S[v[i]] == 1)
+			right.push_back(v[i]);
+	}
+
+	lrt.insert(x + y);
+	minx = min(minx, x);
+	mx = max(x, mx);
+	my = max(y, my);
+
+	pv.push_back(v[0]);
+	dfs(left, nl, pos - 1, x - 1, y + 1);
+	dfs(right, pos + 1, nr, x + 1, y + 1);
+	tv.push_back(v[0]);
+
+	return;
+}
+
+void my_print(vector<int> &v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (i != 0)
+			printf(" ");
+		printf("%d", v[i]);
+	}
+	printf("\n");
+}
